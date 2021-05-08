096b006c001eb2fd1b40946a866daa6358a34187
benhassenwael <greenw2012@gmail.com>
2021-05-08 01:25:06 +0100

    Testing

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" First endpoint route returning the status """
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+import json
+
+app = Flask('v1')
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """Close app storage"""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 not found errors and return json object"""
+    return json.dumps({"error": "Not found"}), 404
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=int(port))
---
+++ api/v1/views/index.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from api.v1.views import app_views
+from models import storage
+import json
+
+
+@app_views.route('/status')
+def get_status():
+    """Returns HTTP status 200"""
+    return json.dumps({"status": "OK"}), 200
+
+@app_views.route('/api/v1/stats')
+def get_stats():
+    """Returns number of each object by type"""
+    objs = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    stats = {name : storage.count(cls) for cls, name in objs.items()}
+    return json.dumps(stats)
